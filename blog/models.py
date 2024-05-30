aca0dff3545ed56736a82f7253d34a1ffbf4d2de
Bekhzod0903 <shbegzod7@gmail.com>
2024-05-30 16:02:29 +0500

    models yozdim

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    class Meta:
+        db_table = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='media/images')
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True,null=True)
+
+    class Meta:
+        db_table = 'posts'
+
+    def __str__(self):
+        return self.title
+
+
+

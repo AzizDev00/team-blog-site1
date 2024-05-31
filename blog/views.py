744070ffb6af3954e570f06c1630ce4eecd53ad6
samandarabduraimov <samandarabduraimov541@gmail.com>
2024-05-31 17:53:08 +0500

    views updated

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,58 @@
-from django.shortcuts import render
-from django.views import ListView
-from django.views.generic.edit import  CreateView , UpdateView , DeleteView 
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from .models import Post ,  Category
 from django.urls import reverse_lazy
 
-# Create your views here.
-class BlogListView(ListView):
-    model = Post
-    template_name = 'blog.html'
 
-class BlogDetailView(ListView):
-    model = Post
-    template_name = 'post_detail.html'
+class CategoriesListView(View):
+    def get(self, request):
+        category = Category.objects.all()
 
-class BlogCreateView(CreateView):
-    model = Post
-    template_name = 'post_create.html'
-    fields = ['title', 'description', 'image']
+        return render(request, 'categories.html', {'categorys': category})
 
-class BlogUpdateView(UpdateView):
-    model = Post
-    template_name = 'post_update.html'
-    fields = ['title', 'description', 'image']
+class BlogListView(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, 'blog.html', {'posts': posts})
 
-class BlogDeleteView(DeleteView):
-    model = Post
-    template_name = 'post_delete.html'
-    success_url = reverse_lazy('blog')
+class BlogDetailView(View):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'post_detail.html', {'post': post})
+
+class BlogCreateView(View):
+    def get(self, request):
+        return render(request, 'post_create.html')
+
+    def post(self, request):
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        image = request.FILES.get('image')
+
+        post = Post(title=title, description=description, image=image)
+        post.save()
+        return redirect('blog')
+
+class BlogUpdateView(View):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'post_update.html', {'post': post})
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.title = request.POST.get('title')
+        post.description = request.POST.get('description')
+        if request.FILES.get('image'):
+            post.image = request.FILES.get('image')
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+
+class BlogDeleteView(View):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'post_delete.html', {'post': post})
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return redirect(reverse_lazy('blog'))
